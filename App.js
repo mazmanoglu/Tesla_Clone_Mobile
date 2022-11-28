775b5823974706db9f85710caf98433e424859f0
fatih ozer <fatihozer246@hotmail.com>
2022-11-28 22:36:57 +0100

    first_push

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, requireNativeComponent, StyleSheet, Text, View } from 'react-native';
+import CarItem from './components/carItems'; //caritem dan sonra index yazmadik cunku bu path in default degeri index.js
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      {/* call the components */}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor:"grey",
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+
